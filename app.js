9c253a5ee17773f7d0e00589bf1c353ba571098b
ashikajith <ashikajith@gmail.com>
2018-01-04 17:08:36 +0530

    Intro to click events and bind events

--- app.js
+++ app.js
@@ -2,12 +2,35 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Ashik',
-    job: 'Ninja'
+    age: '25',
+    job: 'Ninja',
+    website: 'https://www.google.co.in/',
+    websiteTag: '<a href="https://www.google.co.in/"> Google Site</a>'
   },
-
   methods: {
     greet: function (time) {
       return 'Good ' + time  + ' ' + this.name;
+    }
+  }
+});
+
+new Vue({
+  el: '#vue-data-bind',
+  data: {
+    age: 26,
+    x: 0,
+    y: 0
+  },
+  methods: {
+    addYear: function(increment){
+      this.age += increment;
+    },
+    substractYear: function(increment){
+      this.age -= increment;
+    },
+    updateXY: function(event){
+      this.x = event.offsetX;
+      this.y = event.offsetY; 
     }
   }
 });
