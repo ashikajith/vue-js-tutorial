f073093ac3db8b413bfb5476cceb6349cf7462f6
ashikajith <ashikajith@gmail.com>
2018-01-09 12:40:12 +0530

    Bunching Applcation using vue with style binding

--- app.js
+++ app.js
@@ -30,7 +30,90 @@ new Vue({
     },
     updateXY: function(event){
       this.x = event.offsetX;
-      this.y = event.offsetY; 
+      this.y = event.offsetY;
     }
   }
 });
+
+new Vue({
+  el: '#keyboard-event-app',
+  data:{
+
+  },
+  methods:{
+    logName: function(){
+      console.log('you netered your name');
+    },
+    logAge: function(){
+      console.log('enters your wafe');
+    }
+  }
+})
+
+new Vue({
+  el: '#data-bind-app',
+  data:{
+    name: '',
+    age: ''
+  },
+  methods:{
+  }
+})
+
+new Vue({
+  el: '#computed-app',
+  data: {
+    age:20,
+    a: 0,
+    b: 0
+  },
+  methods: {
+
+  },
+  computed:{
+    addToA: function(){
+      console.log('method a');
+      return this.a + this.age;
+    },
+    addToB: function(){
+      console.log('emthod b');
+      return this.b + this.age;
+    }
+  }
+})
+
+new Vue({
+  el: '#dynamic-css',
+  data: {
+    available: false,
+    nearby: false
+  },
+   computed: {
+     compClasses: function(){
+       return {
+         available: this.available,
+         nearby: this.nearby
+       }
+     }
+   }
+})
+
+new Vue({
+  el: '#loop-for',
+  data:{
+    characters: [1,2,3,4,5,6],
+    ninjas: [
+      {name: 'Rian', age: 24},
+      {name: 'Ashik', age: 23},
+      {name: 'Manu', age: 22},
+      {name: 'Hari', age: 21}
+    ]
+  },
+
+  methods:{
+
+  },
+  computed:{
+    
+  }
+})
